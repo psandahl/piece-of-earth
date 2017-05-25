16c8bdbaec157d4dbc1aa37044241ab5cace1cd7
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-05-25 19:44:32 +0200

    Skeleton shaders.

--- src/Graphics/Terrain.hs
+++ src/Graphics/Terrain.hs
@@ -15,22 +15,43 @@ module Graphics.Terrain
     , render
     ) where
 
+import qualified BigE.Program           as Program
+import           BigE.Types             (Program, ShaderType (..))
 import           Control.Monad.IO.Class (MonadIO)
 import           Graphics.GL            (GLfloat)
 import           Linear                 (M44)
 import           Prelude                hiding (init)
+import           System.FilePath        ((</>))
 
 -- | Terrain record.
 data Terrain = Terrain
-    deriving Show
+    { program :: !Program
+      -- ^ The shader program for rendering of terrains.
+    } deriving Show
 
 -- | Initialize the terrain given the path to the resource base directory.
 init :: MonadIO m => FilePath -> m (Either String Terrain)
-init _resourceDir = return $ Right Terrain
+init resourceDir = do
+    eProgram <- loadProgram resourceDir
 
+    case eProgram of
+        Right program' ->
+            return $ Right Terrain { program = program' }
+        Left err -> return $ Left err
+
+-- | Delete the terrains resources.
 delete :: MonadIO m => Terrain -> m ()
-delete _terrain = return ()
+delete terrain =
+    Program.delete $ program terrain
 
 -- | Render all terrain given the perspective/view matrix.
 render :: MonadIO m => M44 GLfloat -> m ()
 render = undefined
+
+loadProgram :: MonadIO m => FilePath -> m (Either String Program)
+loadProgram resourceDir = do
+    let vertexShader = resourceDir </> "shaders" </> "terrain.vert"
+        fragmentShader = resourceDir </> "shaders" </> "terrain.frag"
+    Program.fromFile [ (VertexShader, vertexShader)
+                     , (FragmentShader, fragmentShader)
+                     ]
