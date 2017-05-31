b5a01c280b53352cbb110eae8ad95268f514bff8
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-05-31 13:36:58 +0200

    Simple callback for wireframe toggle.

--- src/Engine/Callback.hs
+++ src/Engine/Callback.hs
@@ -10,13 +10,18 @@ module Engine.Callback
     ) where
 
 import           BigE.Math    (mkPerspective)
-import           BigE.Runtime (Render, modifyAppState, setWindowSizeCallback)
-import           Engine.State (State (..))
+import           BigE.Runtime (Key (..), ModifierKeys, Render,
+                               getAppStateUnsafe, modifyAppState, putAppState,
+                               setKeyPressedCallback, setKeyReleasedCallback,
+                               setWindowSizeCallback)
+import           Engine.State (State (..), UserInput (..))
 
 -- | Install callbacks.
 install :: Render State ()
-install =
+install = do
     setWindowSizeCallback (Just windowSizeCallback)
+    setKeyPressedCallback (Just keyPressedCallback)
+    setKeyReleasedCallback (Just keyReleasedCallback)
 
 -- | Callback to handle application specific stuff when the window's size
 -- is changed. Adjusting the viewport is handled automatically.
@@ -24,3 +29,24 @@ windowSizeCallback :: Int -> Int -> Render State ()
 windowSizeCallback width height =
     modifyAppState $ \state ->
         state { perspective = mkPerspective (width, height) }
+
+-- | Callback to handle user pressing keys.
+keyPressedCallback :: Key -> ModifierKeys -> Render State ()
+
+-- Handle W, wireframe activation.
+keyPressedCallback Key'W _modifierKeys = do
+    state <- getAppStateUnsafe
+    let userInp = userInput state
+
+    if renderWireframe userInp then
+        putAppState $ state { userInput = userInp { renderWireframe = False }}
+    else putAppState $ state { userInput = userInp { renderWireframe = True }}
+
+-- Default - no - action.
+keyPressedCallback _key _modifierKeys  = return ()
+
+-- | Callback to handle user releasing keys.
+keyReleasedCallback :: Key -> ModifierKeys -> Render State ()
+
+-- Default - no - action.
+keyReleasedCallback _key _modifierKeys  = return ()
